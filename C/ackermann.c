353624975f16aa61e62868c7df73a8d378f40820
Neo_Chen <chenkolei@gmail.com>
2019-02-24 22:21:09 +0800

    ackermann: Benchmark?

--- C/ackermann.c
+++ C/ackermann.c
@@ -2,12 +2,18 @@
 #include <stdint.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <limits.h>
+#include <signal.h>
+#include <time.h>
 
 typedef unsigned long long int num_t;
 
+num_t count=0;
+clock_t start;
+clock_t end=0;
+
 num_t ackermann(num_t m, num_t n)
 {
+	count++;
 	if(m == 0)
 		return n+1;
 	if(m > 0 && n == 0)
@@ -18,9 +24,20 @@ num_t ackermann(num_t m, num_t n)
 	return 0; /* Might be possible to reach here? */
 }
 
+void quit(int signal)
+{
+	fprintf(stderr, "\rC=%llu\n", count);
+	fprintf(stderr, "T=%ld\n", clock() - start);
+	fputs("==========\n", stderr);
+	if(signal == SIGQUIT)
+		exit(0);
+}
+
 int main(int argc, char **argv)
 {
 	num_t arg_m, arg_n;
+	num_t ret=0;
+	start = clock();
 
 	if(argc < 3)
 	{
@@ -28,10 +45,16 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 
+	signal(SIGINT, quit);
+	signal(SIGQUIT, quit);
+
 	arg_m = strtoull(argv[1], NULL, 10);
 	arg_n = strtoull(argv[2], NULL, 10);
 
-	printf("ACKERMANN(%llu, %llu) = %llu\n", arg_m, arg_n, ackermann(arg_m, arg_n));
-
+	ret = ackermann(arg_m, arg_n);
+	printf("%ld usec, %llu times called, ACKERMANN(%llu, %llu) = %llu\n",
+			(end = clock()) - start,
+			count, arg_m, arg_n, ret);
+	printf("%lld c/usec\n", count / (end - start));
 	return 0;
 }
