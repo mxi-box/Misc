f52d0faf49781229a72c327fc7e7aa0518d98755
Neo_Chen <chenkolei@gmail.com>
2019-05-09 21:09:02 +0800

    Added random.c

---
+++ C/random.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+	unsigned long long int num=0, i=0;
+
+	srandom(time(NULL));
+	for(i=0; i < (1ULL<<33); i++) num+=random();
+
+	printf("RAND=%llx\n", num);
+	return 0;
+}
