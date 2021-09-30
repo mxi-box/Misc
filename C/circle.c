ec49081887245fe84edf41e40c51469d81820bbf
Neo_Chen <chenkolei@gmail.com>
2021-09-30 09:55:02 +0800

    circle: very fast

--- C/circle.c
+++ C/circle.c
@@ -1,65 +1,75 @@
+/* ==================================== *\
+ * Draw circle with high I/O throughput *
+\* ==================================== */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
+#include <limits.h>
 
 typedef long long int num_t;
 
-double distance(double a, double b)
+static inline double distance(int x, int y)
 {
-	return sqrt(a*a + b*b);
+	return sqrt(x*x/4.0 + y*y);
 }
 
-char const dispchar[] = "%XH=- ";
-
-#ifndef NO_DRAW
-#define DRAW(x)	\
-	putchar(dispchar[(x)])
-#else
-#define DRAW(x)
-#endif
 
 int main(int argc, char **argv)
 {
-	double x, y, d;
+	int x, y;
+	double d;
 	int size;
-	double area = 0.0;
+	int hsize;
+	char *linebuf;
+	long long unsigned int area = 0;
+	int xlast = 0; /* last position of x */
+	int *lastpos;
+	unsigned int areadelta = 0;
 
 	if(argc > 2)
 		goto error;
 
 	size = atoi(argv[1]);
 
-	if(size < 0)
+	if(size < 0 || size >= (INT_MAX / 2))
 		goto error;
 	/* plot size: size * 2 + 1 */
 
+	hsize = size * 4 + 1; // Double horizontal resolution
+	linebuf = malloc(hsize + 1); // include space for NUL
+	if(linebuf == NULL)
+		goto error;
+	linebuf[hsize] = '\n';
+
+	/* Only record the upper half + center line */
+	lastpos = calloc(size + 1, sizeof(int));
+	if(lastpos == NULL)
+		goto error;
+
 	for(y = size; y >= -size; y--)
 	{
-		for(x = -size; x <= size; x += 0.5)
+		memset(linebuf, ' ', hsize);
+
+		/* what an interesting algorithm */
+		for(x = xlast; x >= -size * 2; x += y >= 0 ? -1 : 1)
 		{
 			d = distance(x, y);
 
-			if(d <= size)
-			{
-				DRAW(0);
-				area += 0.5;
-			}
-			else if(d - size <= 0.0625)
-				DRAW(1);
-			else if(d - size <= 0.125)
-				DRAW(2);
-			else if(d - size <= 0.25)
-				DRAW(3);
-			else if(d - size <= 0.50)
-				DRAW(4);
-			else
-				DRAW(5);
+			/* Split because what value to record (before encounter / after encounter) is different */
+			if(y >= 0 && d > size)
+				break;
+			xlast = x;
+			if(y < 0 && d <= size)
+				break;
 		}
-
-		putchar('\n');
+		memset(linebuf + xlast + size * 2, '#', areadelta = abs(xlast) * 2 + 1);
+		area += areadelta;
+		fwrite(linebuf, hsize + 1, sizeof(char), stdout);
 	}
 
-	fprintf(stderr, "\nPi = %lf\n", area / (size * size));
+	fprintf(stderr, "\nPi = %lf\n", (double)area / 2.0 / (size * size));
 	exit(0);
 error:
 	exit(1);
