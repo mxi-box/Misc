82117f81aedc2db98e33d4a802a5708cc7c3f2bc
Neo_Chen <chenkolei@gmail.com>
2021-10-29 00:17:14 +0800

    prime.c: allocate memory dynamically

--- C/prime.c
+++ C/prime.c
@@ -4,17 +4,38 @@
 #include <stdlib.h>
 #include <math.h>
 
-int main(void)
+#define INITIAL_SIZE	65536
+#define ALLOC_STEP	4096
+#define ALLOC_THRESHOLD	1024
+
+#define NULLCHK(x)	\
+	if((x) == NULL)			\
+	{				\
+		puts("Out of memory");	\
+		exit(1);		\
+	}
+
+int main(int argc, char **argv)
 {
 	typedef unsigned long long int num_t;
 	num_t i = 0, j = 1;
-	unsigned int p = 0;
+	size_t p = 0;
+	size_t size = INITIAL_SIZE;
 
 	num_t *primes;
-	num_t num = 65536;
-	primes = malloc(num * sizeof(num_t));
+	num_t num = 1;
+	primes = calloc(size, sizeof(num_t));
+	NULLCHK(primes);
 
 	primes[0] = 2;
+
+	if(argc < 2)
+	{
+		puts("No enough arguments");
+		exit(1);
+	}
+
+	num = strtoull(argv[1], NULL, 10);
 
 next:
 	while(j++ < num)
@@ -24,6 +45,15 @@ next:
 			if(j % primes[i] == 0)	// Is divisible by prime
 				goto next; // The almighty goto
 		}
+
+		if(size - p < ALLOC_THRESHOLD)
+		{
+			primes = realloc(primes, (size += ALLOC_STEP) * sizeof(num_t));
+			NULLCHK(primes);
+		}
+
 		printf("%llu\n", primes[p++] = j);
 	}
+
+	return 0;
 }
