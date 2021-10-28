cd9a9ab1b81d5d0622a07cd46cadc119421339df
Neo_Chen <chenkolei@gmail.com>
2021-10-28 23:46:34 +0800

    prime.c: much better

--- C/prime.c
+++ C/prime.c
@@ -9,28 +9,20 @@ int main(void)
 	typedef unsigned long long int num_t;
 	num_t i = 0, j = 1;
 	unsigned int p = 0;
-	double sqrj;
 
 	num_t *primes;
 	num_t num = 65536;
-	primes = calloc(num, sizeof(num_t));
+	primes = malloc(num * sizeof(num_t));
 
 	primes[0] = 2;
-	p = 1;
 
 next:
 	while(j++ < num)
 	{
-		sqrj = sqrt(j);
-		for(i = 0; i < p && primes[i] < sqrj; i++)
+		for(i = 0; primes[i] <= ceil(sqrt(j)) && i < p; i++)
 		{
-			if(j % primes[i] == 0)
-				goto next;
-		}
-		for(i = primes[i - 1]; i <= sqrj; i++)
-		{
-			if(j % i == 0)
-				goto next;
+			if(j % primes[i] == 0)	// Is divisible by prime
+				goto next; // The almighty goto
 		}
 		printf("%llu\n", primes[p++] = j);
 	}
