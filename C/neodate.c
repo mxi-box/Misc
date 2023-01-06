2b544903a196b6acd2b98076a81523cb033f4ed3
Neo_Chen <chenkolei@gmail.com>
2023-01-06 21:44:05 +0800

    neodate: ISO-8601 longer format

--- C/neodate.c
+++ C/neodate.c
@@ -8,7 +8,7 @@ int main()
 	char c[4096];
 	const time_t time_now = time(NULL);
 	const struct tm *tm = localtime(&time_now);
-	strftime(c, sizeof(c), "%Y%m%dT%H%M%S%z", tm);
+	strftime(c, sizeof(c), "%Y-%m-%dT%H:%M:%S%z", tm);
 	puts(c);
 	return 0;
 }
