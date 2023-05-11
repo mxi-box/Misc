8fb9aafffd08d2c5ac1fe25cf0e4636e17c5a82a
Neo_Chen <chenkolei@gmail.com>
2023-05-12 00:29:45 +0800

    C: e now uses 64bit long division

--- C/e.c
+++ C/e.c
@@ -1,69 +1,169 @@
 /* Calculate e with some bignum magic */
+/* NOTE: index 0 is the highest word */
+
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <math.h>
 #include <float.h>
-#include <mpfr.h>
+#include <inttypes.h>
 
-typedef long long unsigned int num_t;
+#define WORD_SIZE (64)
+
+typedef uint64_t num_t;
+#if __SIZEOF_INT128__ != 16
+	#error "No 128-bit integer support"
+#endif
+typedef __uint128_t num128_t; // necessary
 typedef uint8_t byte;
 
-double log2factorial(num_t n)
+num_t divisor_block[WORD_SIZE][2];
+#define LAST_DIVISOR_BLOCK (WORD_SIZE - 1)
+
+double log2fractorial(num_t n)
 {
 	const double h = log2(2 * M_PI)/2;
 	return h + log2(10 * n) * (n + 0.5) - n / log(2);
 }
 
-num_t byte_to_digits(size_t n)
+void dump_frac(num_t *frac, num_t n)
 {
-	const size_t digits = floor(log(2)/log(10) * n * 8);
+	for(size_t i = 0; i < n; i++)
+	{
+		printf("_%016" PRIx64, frac[i]);
+	}
+	putchar('\n');
+}
+
+num_t to_digits_precision(size_t n, size_t word_size)
+{
+	const size_t digits = floor(log(2)/log(10) * n * word_size);
 	printf("digits -> %zu\n", digits);
 	return digits;
 }
 
+static inline num_t intpow10(num_t n)
+{
+	num_t r = 1;
+	for(num_t i = 0; i < n; i++)
+		r *= 10;
+	return r;
+}
+
+#define GROUP_SIZE (19)
+const num_t pow10_19 = 10000000000000000000ULL;
+
+void print_fraction(num_t *frac, size_t n, size_t digits)
+{
+	// in groups of digits
+	for(size_t i = 0; i < digits/GROUP_SIZE; i++)
+	{
+		num128_t tmp128b = 0;
+		for(ssize_t j = n - 1; j >= 0; j--)
+		{
+			tmp128b = (num128_t)frac[j] * pow10_19 + tmp128b;
+			frac[j] = tmp128b; // lower 64 bits
+			tmp128b >>= 64; // carry
+			//dump_frac(efrac, n);
+		}
+		printf("%019" PRIu64, (num_t)(tmp128b));
+	}
+
+	// the rest in one group
+	size_t rest = digits % GROUP_SIZE;
+	char fmtspec[16];
+
+	num128_t tmp128b = 0;
+	for(ssize_t j = n - 1; j >= 0; j--)
+	{
+		tmp128b = (num128_t)frac[j] * intpow10(rest) + tmp128b;
+		frac[j] = tmp128b; // lower 64 bits
+		tmp128b >>= 64;
+		//dump_frac(efrac, n);
+	}
+	sprintf(fmtspec, "%%0%zu" PRIu64, rest);
+	printf(fmtspec, (num_t)(tmp128b));
+}
+
+/* Algorithm by Steve Wozniak in 1980
+ * https://archive.org/details/byte-magazine-1981-06/page/n393/mode/1up
+ */
+
+// TODO: remove requirement for 128-bit integers here as it's not "that" necessary in the strict sense
+// Or maybe not, depending on which implementation is faster
+void fraction_div_add1(num_t *frac, size_t n, num_t divisor_block[][2])
+{
+	num_t remainder = 1;	// the implicit 1 at the beginning
+	for(size_t i = 0; i < n; i++)
+	{
+		num_t tmp_quotient = 0;
+		num128_t tmp_partial_dividend = (num128_t)remainder << 64 | frac[i];
+		num128_t tmp_divosor;
+
+		for(int j = LAST_DIVISOR_BLOCK; j >= 0; j--)
+		{
+			tmp_divosor = (num128_t)divisor_block[j][0] << 64 | divisor_block[j][1];
+			if(tmp_partial_dividend >= tmp_divosor)
+			{
+				tmp_quotient |= 1ULL << j;
+				tmp_partial_dividend -= tmp_divosor;
+			}
+		}
+		frac[i] = tmp_quotient;
+		remainder = tmp_partial_dividend; // lower 64bits
+	}
+}
+
 int main(int argc, char **argv)
 {
-	num_t n = 1024;
-	int fpif_export = 0;
+	num_t terms = 5;
 	if(argc == 2)
+		terms = atoll(argv[1]);
+
+	double precision = log2fractorial(terms);
+	printf("estimated required precision: log2(%" PRIu64 "!) ~= %lfbits\n", terms, precision);
+	
+	size_t efrac_size = ceil(precision / WORD_SIZE);
+	num_t *efrac = calloc(efrac_size, sizeof(num_t));
+	printf("allocated %" PRIu64 " 64bit words (%zu bit)\n", efrac_size, efrac_size * WORD_SIZE);
+
+	// divisor = 1 is impossible as we don't really store the integer part
+	// TODO: no need to split the divisor block into two parts if using 128-bit integers
+	for(num_t divisor = terms; divisor > 1; divisor--)
 	{
-		if(argv[1][0] == '-')
+		//printf("======\ndivisor -> %" PRIu64 " (%#" PRIx64 ")\n", divisor, divisor);
+
+		// Initialize the divisor block
+		for(int i = 0; i < WORD_SIZE; i++)
 		{
-			fpif_export = 1;
-			argv[1]++;
+			divisor_block[i][1] = divisor << i;
+			if(i > 0)
+				divisor_block[i][0] = divisor >> (64 - i);
+			else
+				divisor_block[i][0] = 0;
+
+			//printf("divisor_block[%d] -> %016" PRIx64 "_%016" PRIx64 "\n", i, divisor_block[i][0], divisor_block[i][1]);
 		}
-		n = atoll(argv[1]);
-	}
-	mpfr_prec_t prec = ceil(log2factorial(n));
 
-	if(prec > MPFR_PREC_MAX || prec < MPFR_PREC_MIN)
-	{
-		mpfr_fprintf(stderr, "precision %Pu is out of range (%Pu to %Pu)\n", prec, MPFR_PREC_MIN, MPFR_PREC_MAX);
-		return 1;
+		fraction_div_add1(efrac, efrac_size, divisor_block);
+		//dump_frac(efrac, efrac_size);
 	}
 
-	mpfr_t e;
-	mpfr_init2(e, prec);
-	fprintf(stderr, "estimated precision -> %lu bits (%lf digits)\n", prec, prec * log10(2));
-	mpfr_set_ui(e, 1, MPFR_RNDN);
+	// how many digits do we have?
 
-	for(num_t i = n; i > 0; i--)
+	/*
+	// Fraction conversion test
+	for(int i = 0; i < n; i++)
 	{
-		mpfr_div_ui(e, e, i, MPFR_RNDN);
-		mpfr_add_ui(e, e, 1, MPFR_RNDN);
+		efrac[i] = 0x1234567890abcdef;
 	}
+	*/
+	size_t digits = to_digits_precision(efrac_size, sizeof(num_t));
 
-	if(fpif_export)
-	{
-		mpfr_fpif_export(stdout, e);
-	}
-	else
-	{
-		puts("e = ");
-		mpfr_out_str(stdout, 10, 0, e, MPFR_RNDN);
-		puts("\n");
-	}
+	// Print the result
+	printf("e = 2.");
+	print_fraction(efrac, efrac_size, digits);
+	putchar('\n');
 	return 0;
 }
