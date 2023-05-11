aebdc0db06953dfe9f1bbfb7bfee85deeaeb92de
Neo_Chen <chenkolei@gmail.com>
2023-05-11 15:10:25 +0800

    C: e: use MPFR

--- C/e.c
+++ C/e.c
@@ -5,34 +5,15 @@
 #include <stdint.h>
 #include <math.h>
 #include <float.h>
+#include <mpfr.h>
 
 typedef long long unsigned int num_t;
-typedef __uint128_t num128_t; // useful when we need to multiply two 64-bit numbers
 typedef uint8_t byte;
 
-num_t divisor_block[64][2];
-
-double log10fractorial(num_t n)
+double log2factorial(num_t n)
 {
-	const double h = log10(2 * M_PI)/2;
-	return h + log10(10 * n) * (n + 0.5) - n / log(10);
-}
-
-int highest_bitpos(num_t n)
-{
-	int r = 0;
-	while(n >>= 1)
-		r++;
-	return r + 1;
-}
-
-void dump_frac(num_t *frac, num_t n)
-{
-	for(int i = 0; i < n; i++)
-	{
-		printf("_%016llx", frac[i]);
-	}
-	putchar('\n');
+	const double h = log2(2 * M_PI)/2;
+	return h + log2(10 * n) * (n + 0.5) - n / log(2);
 }
 
 num_t byte_to_digits(size_t n)
@@ -44,67 +25,45 @@ num_t byte_to_digits(size_t n)
 
 int main(int argc, char **argv)
 {
-	num_t n = 5;
-	//if(argc == 2)
-	//	n = atoll(argv[1]);
-	num_t *efrac = calloc(n, sizeof(num_t));
-	printf("n -> %llu\n", n);
-	
-	/*
-	// index 0 is highest word
-	for(num_t divisor = n; divisor > 0; divisor--)
+	num_t n = 1024;
+	int fpif_export = 0;
+	if(argc == 2)
 	{
-		printf("======\ndivisor -> %llu (%#llx)\n", divisor, divisor);
-
-		// Initialize the divisor block
-		for(int i = 0; i < 64; i++)
+		if(argv[1][0] == '-')
 		{
-			divisor_block[i][1] = divisor << i;
-			if(i > 0)
-				divisor_block[i][0] = divisor >> (64 - i);
-			else
-				divisor_block[i][0] = 0;
-
-			printf("divisor_block[%d] -> %016llx_%016llx\n", i, divisor_block[i][0], divisor_block[i][1]);
+			fpif_export = 1;
+			argv[1]++;
 		}
-
-		// Do the division, assume a hidden 1 in front of the fraction part
-		// i.e. always assume the first dividand "block" is larger than divisor
-
+		n = atoll(argv[1]);
 	}
-	*/
+	mpfr_prec_t prec = ceil(log2factorial(n));
 
-	// how many digits do we have?
-
-	/*
-	// Fraction conversion test
-	for(int i = 0; i < n; i++)
+	if(prec > MPFR_PREC_MAX || prec < MPFR_PREC_MIN)
 	{
-		efrac[i] = 0x1234567890abcdef;
+		mpfr_fprintf(stderr, "precision %Pu is out of range (%Pu to %Pu)\n", prec, MPFR_PREC_MIN, MPFR_PREC_MAX);
+		return 1;
 	}
-	*/
-	size_t digits = byte_to_digits(n * sizeof(num_t));
 
-	// Print the result
-	printf("e = 2.");
-	for(num_t i = 0; i < digits; i++)
+	mpfr_t e;
+	mpfr_init2(e, prec);
+	fprintf(stderr, "estimated precision -> %lu bits (%lf digits)\n", prec, prec * log10(2));
+	mpfr_set_ui(e, 1, MPFR_RNDN);
+
+	for(num_t i = n; i > 0; i--)
 	{
-		for(int j = n - 1; j >= 0; j--)
-		{
-			num128_t tmp = (num128_t)efrac[j] * (num128_t)10;
-			efrac[j] = tmp & 0xffffffffffffffff; // lower 64 bits
-			if(j > 0)
-			{
-				efrac[j - 1] += tmp >> 64; // upper 64 bits
-			}
-			else
-			{
-				// print the overflowed digit
-				printf("%01hhu", (byte)(tmp >> 64));
-			}
-			//dump_frac(efrac, n);
-		}
+		mpfr_div_ui(e, e, i, MPFR_RNDN);
+		mpfr_add_ui(e, e, 1, MPFR_RNDN);
 	}
-	putchar('\n');
+
+	if(fpif_export)
+	{
+		mpfr_fpif_export(stdout, e);
+	}
+	else
+	{
+		puts("e = ");
+		mpfr_out_str(stdout, 10, 0, e, MPFR_RNDN);
+		puts("\n");
+	}
 	return 0;
 }
