ef2bef1b56ed0bb98dbc2b838aca4287ed87fb31
Neo_Chen <chenkolei@gmail.com>
2023-05-12 10:59:24 +0800

    e.c: even faster when we use 32bit word size

--- C/e.c
+++ C/e.c
@@ -10,92 +10,88 @@
 #include <inttypes.h>
 #include <assert.h>
 
-#define WORD_SIZE (64)
+#define WORD_SIZE (32)
 
-typedef uint64_t num_t;
-#if __SIZEOF_INT128__ != 16
-	#error "No 128-bit integer support"
-#endif
-typedef __uint128_t num128_t; // necessary
+typedef uint32_t word_t;
+typedef uint64_t dword_t; // necessary
 typedef uint8_t byte;
 
 // Calculating the accurate value of log2(n!) isn't that prohibitive on modern hardware
-double log2fractorial(num_t n)
+double log2fractorial(word_t n)
 {
 	return log2(2 * M_PI)/2 + log2(n) * (n + 0.5) - n / log(2);
 }
 
-void dump_frac(num_t *frac, num_t n)
+void dump_frac(word_t *frac, word_t n)
 {
 	for(size_t i = 0; i < n; i++)
 	{
-		printf("_%016" PRIx64, frac[i]);
+		printf("_%08" PRIx32, frac[i]);
 	}
 	putchar('\n');
 }
 
-num_t to_digits_precision(size_t n, size_t word_size)
+word_t to_digits_precision(size_t n, size_t word_size)
 {
 	const size_t digits = floor(log(2)/log(10) * n * word_size);
 	printf("digits -> %zu\n", digits);
 	return digits;
 }
 
-static inline num_t intpow10(num_t n)
+static inline word_t intpow10(word_t n)
 {
-	num_t r = 1;
+	word_t r = 1;
 	assert(n <= 19);
-	for(num_t i = 0; i < n; i++)
+	for(word_t i = 0; i < n; i++)
 		r *= 10;
 	return r;
 }
 
-#define GROUP_SIZE (19)
-const num_t pow10_19 = 10000000000000000000ULL;
+#define GROUP_SIZE (9)
+const word_t pow10_9 = 1000000000ULL;
 
-void print_fraction(num_t *frac, size_t n, size_t digits)
+void print_fraction(word_t *frac, size_t n, size_t digits)
 {
 	// in groups of digits
 	for(size_t i = 0; i < digits/GROUP_SIZE; i++)
 	{
-		num128_t tmp128b = 0;
+		dword_t tmp64b = 0;
 		for(ssize_t j = n - 1; j >= 0; j--)
 		{
-			tmp128b = (num128_t)frac[j] * pow10_19 + tmp128b;
-			frac[j] = tmp128b; // lower 64 bits
-			tmp128b >>= 64; // carry
+			tmp64b = (dword_t)frac[j] * pow10_9 + tmp64b;
+			frac[j] = tmp64b; // lower 64 bits
+			tmp64b >>= WORD_SIZE; // carry
 			//dump_frac(efrac, n);
 		}
-		printf("%019" PRIu64, (num_t)(tmp128b));
+		printf("%09" PRIu32, (word_t)(tmp64b));
 	}
 
 	// the rest in one group
 	size_t rest = digits % GROUP_SIZE;
 	char fmtspec[16];
 
-	num128_t tmp128b = 0;
+	dword_t tmp64b = 0;
 	for(ssize_t j = n - 1; j >= 0; j--)
 	{
-		tmp128b = (num128_t)frac[j] * intpow10(rest) + tmp128b;
-		frac[j] = tmp128b; // lower 64 bits
-		tmp128b >>= 64;
+		tmp64b = (dword_t)frac[j] * intpow10(rest) + tmp64b;
+		frac[j] = tmp64b; // lower 64 bits
+		tmp64b >>= WORD_SIZE;
 		//dump_frac(efrac, n);
 	}
-	sprintf(fmtspec, "%%0%zu" PRIu64, rest);
-	printf(fmtspec, (num_t)(tmp128b));
+	sprintf(fmtspec, "%%0%zu" PRIu32, rest);
+	printf(fmtspec, (word_t)(tmp64b));
 }
 
 /* Algorithm by Steve Wozniak in 1980
  * https://archive.org/details/byte-magazine-1981-06/page/n393/mode/1up
  */
 
-// Use 128bit division offered by compilers is actually faster
-void fraction_div_add1(num_t *frac, size_t n, num_t divisor)
+static inline void fraction_div_add1(word_t *frac, size_t n, word_t divisor)
 {
-	num128_t tmp_partial_dividend = 1;
+	dword_t tmp_partial_dividend = 1;
 	for(size_t i = 0; i < n; i++)
 	{
-		num_t tmp_quotient = 0;
+		word_t tmp_quotient = 0;
 		tmp_partial_dividend <<= WORD_SIZE;
 		tmp_partial_dividend |= frac[i];
 		tmp_quotient = tmp_partial_dividend / divisor;
@@ -107,24 +103,27 @@ void fraction_div_add1(num_t *frac, size_t n, num_t divisor)
 
 int main(int argc, char **argv)
 {
-	num_t terms = 5;
+	word_t terms = 5;
 	if(argc == 2)
-		terms = atoll(argv[1]);
+		sscanf(argv[1], "%" SCNu32, &terms);
 
 	double precision = log2fractorial(terms);
-	printf("estimated required precision: log2(%" PRIu64 "!) ~= %lfbits\n", terms, precision);
+	fprintf(stderr, "estimated required precision: log2(%" PRIu32 "!) ~= %lfbits\n", terms, precision);
 	
 	size_t efrac_size = ceil(precision / WORD_SIZE);
-	num_t *efrac = calloc(efrac_size, sizeof(num_t));
-	printf("allocated %" PRIu64 " 64bit words (%zu bit)\n", efrac_size, efrac_size * WORD_SIZE);
+	word_t *efrac = calloc(efrac_size, sizeof(word_t));
+	fprintf(stderr, "allocated %zd %dbit words (%zu bit)\n", efrac_size, WORD_SIZE, efrac_size * WORD_SIZE);
 
 	// divisor = 1 is impossible as we don't really store the integer part
 	// TODO: no need to split the divisor block into two parts if using 128-bit integers
-	for(num_t divisor = terms; divisor > 1; divisor--)
+	word_t div_percent = (terms - 1)/100;
+	for(word_t divisor = terms; divisor > 1; divisor--)
 	{
 		fraction_div_add1(efrac, efrac_size, divisor);
-		//dump_frac(efrac, efrac_size);
+		if(divisor % (div_percent+1) == 0) // +1 to avoid division by zero
+			fprintf(stderr, ">%3" PRIu32 "%% done\r", (terms - divisor)/(div_percent+1));
 	}
+	putc('\n', stderr);
 
 	// how many digits do we have?
 	size_t digits = to_digits_precision(efrac_size, WORD_SIZE);
