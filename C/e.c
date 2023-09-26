ae305c5bc0174148191210098fe4656e4ec59538
Neo_Chen <chenkolei@gmail.com>
2023-09-27 00:40:07 +0800

    e.c: parallelized with OpenMP

--- C/e.c
+++ C/e.c
@@ -11,6 +11,7 @@
 #include <float.h>
 #include <inttypes.h>
 #include <assert.h>
+#include <omp.h>
 
 #define true	1
 #define false	0
@@ -90,48 +91,105 @@ void print_fraction(word_t *frac, size_t n, size_t digits)
 	printf(fmtspec, (word_t)(wide));
 }
 
-volatile word_t current_divisor = 0;
+volatile word_t ctr = 0;
 word_t terms = 5;
 
 void display(union sigval sigval)
 {
-	clock_t ticks = clock();
-	word_t current = terms - current_divisor;
-	static clock_t last_ticks = 0;
 	static word_t last = 0;
-	if(last_ticks == 0)
-	{
-		last_ticks = ticks;
-		return;
-	}
 	fprintf(stderr, ">%7.3f%% (%" PRIu64 "/%" PRIu64 ") @%lluT/s\n",
-		(float)current * 100 / terms,
-		current,
+		(float)ctr * 100 / terms,
+		ctr,
 		terms,
-		(long long unsigned int)((current - last)*CLOCKS_PER_SEC/(ticks - last_ticks + 1))
+		(long long unsigned int)((ctr - last))
 	);
-	last = current;
-	last_ticks = ticks;
+	last = ctr;
 }
 
 /* Algorithm by Steve Wozniak in 1980
  * https://archive.org/details/byte-magazine-1981-06/page/n393/mode/1up
  */
 
+ static inline word_t frac(word_t *efrac, size_t start, size_t end, word_t divisor, word_t remainder)
+ {
+	if(divisor == 1 || divisor <= 1 || start == end)
+		return 0;
+	dword_t tmp_partial_dividend = remainder;
+	//#pragma omp parallel for private(tmp_partial_dividend)
+	for(size_t i = start; i < end; i++)
+	{
+		tmp_partial_dividend = (tmp_partial_dividend << WORD_SIZE) | efrac[i];
+		efrac[i] = tmp_partial_dividend / divisor;
+		tmp_partial_dividend %= divisor;
+	}
+	return (word_t)tmp_partial_dividend;
+ }
+
+ static inline void ecalc_parallel(size_t efrac_size, word_t *efrac, word_t *remainders_in, word_t *remainders_out, word_t *divisors_pipeline)
+ {
+	#pragma omp parallel shared(remainders_in, remainders_out, efrac, efrac_size, divisors_pipeline)
+	{
+		int t = omp_get_thread_num();
+		size_t chunk_size = efrac_size / omp_get_num_threads();
+		size_t start = t * chunk_size;
+		size_t end = (t + 1) * chunk_size;
+		if(t == omp_get_num_threads() - 1)
+			end = efrac_size;
+		remainders_out[t] = frac(efrac, start, end, divisors_pipeline[t], remainders_in[t]);
+
+		#pragma omp barrier
+		#pragma omp single
+		{
+			for(size_t i = 1; i < omp_get_max_threads(); i++)
+			{
+				remainders_in[i] = remainders_out[i - 1];
+			}
+		}
+	}
+ }
+
 // TODO: try to parallelize this
 static inline void ecalc(word_t *efrac, size_t efrac_size, word_t terms)
 {
+	int maxt = omp_get_max_threads();
+	fprintf(stderr, "calculating e with %d threads\n", maxt);
+	word_t divisors_pipeline[maxt];
+	word_t remainders_in[maxt];
+	word_t remainders_out[maxt];
+
+	// initialize the pipeline
+	for(size_t i = 0; i < maxt; i++)
+		divisors_pipeline[i] = 0;
+
+	remainders_in[0] = 1;
+	for(size_t i = 1; i < maxt; i++)
+		remainders_in[i] = 0;
+
 	// divisor = 1 is impossible as we don't really store the integer part
 	for(word_t divisor = terms; divisor > 1; divisor--)
 	{
-		dword_t tmp_partial_dividend = 1;
-		for(size_t i = 0; i < efrac_size; i++)
-		{
-			tmp_partial_dividend = (tmp_partial_dividend << WORD_SIZE) | efrac[i];
-			efrac[i] = tmp_partial_dividend / divisor;
-			tmp_partial_dividend %= divisor;
-		}
-		current_divisor = divisor;
+		for(size_t i = maxt - 1; i > 0; i--)
+			divisors_pipeline[i] = divisors_pipeline[i - 1];
+		divisors_pipeline[0] = divisor;
+
+		// print out the array
+		/*
+		fprintf(stderr, "divisor =\t");
+		for(int i = 0; i < maxt; i++)
+			fprintf(stderr, "%" PRIu64 "\t", divisors_pipeline[i]);
+		fprintf(stderr, "\n");
+		*/
+		ecalc_parallel(efrac_size, efrac, remainders_in, remainders_out, divisors_pipeline);
+		ctr++;
+	}
+
+	// finish the pipeline
+	for(int i = 0; i < maxt; i++)
+	{
+		for(size_t i = maxt - 1; i > 0; i--)
+			divisors_pipeline[i] = divisors_pipeline[i - 1];
+		divisors_pipeline[0] = 0;
+		ecalc_parallel(efrac_size, efrac, remainders_in, remainders_out, divisors_pipeline);
 	}
 }
 
@@ -185,7 +243,7 @@ int main(int argc, char **argv)
 		.it_interval.tv_nsec=000000000L
 	};
 
-	current_divisor = terms;
+	ctr = 0;
 	timer_settime(timer, TIMER_ABSTIME, &period, NULL);
 
 	// calculate e
